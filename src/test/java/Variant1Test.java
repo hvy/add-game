708e4527b9afa57b33a914ec2248559fbfe4bf54
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2016-01-17 12:46:14 +0900

    Finished NVDA Variant 1 tests.

--- src/test/java/Variant1Test.java
+++ src/test/java/Variant1Test.java
@@ -28,15 +28,15 @@ public class Variant1Test {
 
     private static List<Integer> sequence; // Initial question.
     private static List<Integer> correctSequence; // The correct answer given the initial sequence and the X.
-    private static List<Integer> dfZeroGuessSequences; // The correct answer.
-    private static List<Integer> dfOneGuessSequences; // Wrong answer example.
+    private static List<Integer> differByZeroGuessSequences; // The correct answer.
+    private static List<Integer> differByOneGuessSequence; // Wrong answer example.
 
     @BeforeClass
     public static void setUpTests() throws Exception {
         sequence = Arrays.asList(1, 2, 3, 4);
         correctSequence = AGNumberSequenceChecker.createModifiedList(sequence, X);
-        dfZeroGuessSequences = Arrays.asList(4, 5, 6, 7);
-        dfOneGuessSequences = Arrays.asList(4, 5, 6, 8);
+        differByZeroGuessSequences = Arrays.asList(4, 5, 6, 7);
+        differByOneGuessSequence = Arrays.asList(4, 5, 6, 8);
     }
 
     @Before
@@ -52,15 +52,55 @@ public class Variant1Test {
 
     @Test
     public void testCase1() throws Exception {
-        game.handleRoundEnd(correctSequence, dfZeroGuessSequences, THINKING_TIME_LIMIT_MS, 0);
+        game.handleRoundEnd(correctSequence, differByZeroGuessSequences, THINKING_TIME_LIMIT_MS, 0);
         assertEquals(game.getCurrentScore(), SEQUENCE_LENGTH);
         assertFalse(game.isTimedOut());
     }
 
     @Test
+    public void testCase2() throws Exception {
+        // N/A
+    }
+
+    @Test
     public void testCase3() throws Exception {
-        game.handleRoundEnd(correctSequence, dfZeroGuessSequences, THINKING_TIME_LIMIT_MS, THINKING_TIME_LIMIT_MS);
+        game.handleRoundEnd(correctSequence, differByZeroGuessSequences, THINKING_TIME_LIMIT_MS, THINKING_TIME_LIMIT_MS);
         assertEquals(game.getCurrentScore(), SEQUENCE_LENGTH);
         assertFalse(game.isTimedOut());
     }
+
+    @Test
+    public void testCase4() throws Exception {
+        game.handleRoundEnd(correctSequence, differByZeroGuessSequences, THINKING_TIME_LIMIT_MS, THINKING_TIME_LIMIT_MS + 1);
+        assertEquals(game.getCurrentScore(), 0);
+        assertTrue(game.isTimedOut());
+    }
+
+    @Test
+    public void testCase5() throws Exception {
+        // Duplicate of test case 3
+    }
+
+    @Test
+    public void testCase6() throws Exception {
+        // Duplicate of test case 3
+    }
+
+    @Test
+    public void testCase7() throws Exception {
+        game.handleRoundEnd(correctSequence, differByOneGuessSequence, THINKING_TIME_LIMIT_MS, THINKING_TIME_LIMIT_MS);
+        assertEquals(game.getCurrentScore(), 0);
+        assertFalse(game.isTimedOut());
+    }
+
+    @Test
+    public void testCase8() throws Exception {
+       // N/A
+    }
+
+    @Test
+    public void testCase9() throws Exception {
+        // Duplicate of test case 3
+    }
 }
+
